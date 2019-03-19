fbcda06f04cbb70d6a2627305ae02f56c3fb0501
binglewei <396883227@qq.com>
2019-03-19 11:22:45 +0800

    updata weixin

--- python_weixin/views.py
+++ python_weixin/views.py
@@ -5,13 +5,13 @@ from django.shortcuts import render
 from django.views.generic import View
 # Create your views here.
 # from  templates import *
-# class login(View):
-def login(request):
-    return render(request,'templates/login.html')
-    # def post(self,request):
-    #     user_input = []
-    #     user  = request.POST.get('user',None)
-    #     email = request.POST.get('email',None)
-    #     temp = {'user':user,'email':email}
-    #     USER_INPUT.append(temp)
-    #     return render(request,'login.html',{'data':USER_INPUT})
+class login(View):
+    def get(self,request):
+        return render(request,'login.html')
+    def post(self,request):
+        USER_INPUT = []
+        user  = request.POST.get('user',None)
+        email = request.POST.get('email',None)
+        temp = {'user':user,'email':email}
+        USER_INPUT.append(temp)
+        return render(request,'login.html',{'data':USER_INPUT})
