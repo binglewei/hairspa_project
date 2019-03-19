44a4a2cd4345871f087366ce6ce0654ff9268973
binglewei <396883227@qq.com>
2019-03-19 20:44:04 +0800

    调试成功

--- python_weixin/views.py
+++ python_weixin/views.py
@@ -4,13 +4,15 @@ from django.views.generic import View
 
 def login(request):
     method=request.method
+    print  "method====",method
+    print  "request.mate====",request.META
     if method=="POST":
         USER_INPUT = []
         user  = request.POST.get('user',None)
         email = request.POST.get('email',None)
         temp = {'user':user,'email':email}
         USER_INPUT.append(temp)
-        return render(request,'login.html',{'data':USER_INPUT})
+        return render(request,'python_weixin/login.html',{'data':USER_INPUT})
     else:
         return render(request,'python_weixin/login.html')
 
