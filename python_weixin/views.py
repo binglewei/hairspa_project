bdd04e70fba9b31e5d76b512033d0c069836d743
binglewei <396883227@qq.com>
2019-03-20 10:28:15 +0800

    修改 gitignore

--- python_weixin/views.py
+++ python_weixin/views.py
@@ -4,6 +4,7 @@ from django.views.generic import View
 
 from django.shortcuts import *
 from response import JSONResponse
+import hashlib
 def login(request):
     method=request.method
     print  "method====",method
@@ -25,4 +26,24 @@ def checkSignature(request):
         key=i[0]
         values=i[1]
         respones_param[key]=values
+    signature=request.GET.get("signature",None)
+    timestamp=request.GET.get("timestamp",None)
+    nonce=request.GET.get("nonce",None)
+    echostr=request.GET.get("echostr",None)
+    token="binglewei"
+    # 字典序排序
+    list = [token, timestamp, nonce]
+    list.sort()
+    sha1 = hashlib.sha1()
+    map(sha1.update, list)
+    hashcode = sha1.hexdigest()
+    # sha1加密算法
+
+    # 如果是来自微信的请求，则回复echostr
+    if hashcode == signature:
+        return echostr
+    sorted_x = sorted(respones_param.iteritems(), key=lambda param: param[0])
+    str_data=""
+    for tuple in sorted_x:
+        str_data += str(tuple[0]) + str(tuple[1])
     return JSONResponse(respones_param)
